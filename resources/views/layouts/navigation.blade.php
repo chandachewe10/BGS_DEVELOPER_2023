445e83b1d06683add7c10d912fa3fffccda58c24
chandachewe10 <chewec03@gmail.com>
2023-04-21 18:10:28 +0100

    Added Add User Controller

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,16 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
+                   BGS
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                     <span style="color:black">{{ __('Dashboard') }}</span>   
+                    </x-nav-link>
+                    <x-nav-link :href="route('add_user')" :active="request()->routeIs('register')">
+                    <span style="color:black"> {{ __('Add User') }}</span>
                     </x-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AddUserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ToDoListController;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/task', ToDoListController::class);
+    Route::get('/add_user', [AddUserController::class.'add_user'])->name('add_user');
+    Route::post('/add_user', [AddUserController::class.'store_user'])->name('store_user');
 });
 
 require __DIR__.'/auth.php';
