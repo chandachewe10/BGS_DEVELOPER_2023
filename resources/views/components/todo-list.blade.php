fae7118eb4360866817c7564c70950c0e9217bb5
chandachewe10 <chewec03@gmail.com>
2023-04-21 12:54:27 +0100

    Added ToDoList Component

---
+++ resources/views/components/todo-list.blade.php
@@ -0,0 +1,54 @@
+<section class="vh-100 gradient-custom-2">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-12 col-xl-10">
+
+        <div class="card mask-custom">
+          <div class="card-body p-4 text-white">
+
+            <div class="text-center pt-3 pb-2">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp"
+                alt="Check" width="60">
+              <h2 class="my-4">Task List</h2>
+            </div>
+
+
+
+            <table class="table text-white mb-0">
+              <thead>
+                <tr>
+                  
+                  <th scope="col">Task</th>
+                  <th scope="col">Due Date</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="fw-normal">
+                 
+                  <td class="align-middle">
+                    <span>Call Sam For payments</span>
+                  </td>
+                  <td class="align-middle">
+                    <h6 class="mb-0"><span class="badge bg-danger">12 May 2023</span></h6>
+                  </td>
+                  <td class="align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+               
+                </tr>
+              </tbody>
+            </table>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
